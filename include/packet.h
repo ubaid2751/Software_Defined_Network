0cf7aaeba1659d1ad39b216bbf8c1deb46d35da0
ubaid2751 <ubaid2751@gmail.com>
2024-10-18 23:28:18 +0530

    initialized

---
+++ include/packet.h
@@ -0,0 +1,17 @@
+#ifndef PACKET_H
+#define PACKET_H
+
+#include<string>
+using namespace std;
+
+class Packet {
+    string source_ip;
+    string destination_ip;
+    string protocol_type;
+    int source_port;
+    int destination_port;
+
+    
+};
+
+#endif // PACKET_H
